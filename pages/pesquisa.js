1b69299ae80648f3a26d611d1b2a4902563dc118
Paulo Vaz <psgvaz@gmail.com>
2020-08-01 20:10:19 -0300

    Correções de Responsividade

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -50,7 +50,7 @@ const Pesquisa = ()  => {
     <div className="pt-6">
       <PageTitle title="Pesquisa"/>      
       <h1 className="text-center font-bold my-4 text-2xl">Criticas e sugestões</h1>
-        <p className="text-center mb-6">
+        <p className="text-center mb-6 ml-6 mr-6">
           O restaurante X sempre busca por atender melhor seus clientes. <br/>
           Por isso, estamos sempre abertos a ouvir a sua opinião
         </p>
